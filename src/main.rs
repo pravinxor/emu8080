06f23b2e7f574450905fb2fad19047e7d217f7a1
Pravin Ramana <pravinramana25@protonmail.ch>
2024-06-27 20:06:49 -0500

    Support reading file and dumping the disassembly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::io::{self, BufReader, Read};
+
+use disassembler::Disassembler;
+
 mod disassembler;
 
 fn main() {
-    println!("Hello, world!");
+    let f = io::stdin();
+    let reader = BufReader::new(f);
+
+    let instructions = Disassembler::new(reader.bytes().flatten());
+    for i in instructions {
+        dbg!(i);
+    }
 }
