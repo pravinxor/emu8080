92275305264b802f19f1c3c3eb164521081c7768
Pravin Ramana <pravinramana25@protonmail.ch>
2024-07-04 12:59:19 -0500

    dont explicitly make segments, just stop if the instruction pointer goes out of the address space

--- src/processor.rs
+++ src/processor.rs
@@ -4,9 +4,6 @@ struct State {
     memory: [u8; 1 << 16],
     registers: [u8; 8],
     instruction_pointer: u16,
-
-    /// Stores the end of the instruction segment
-    instruction_segment: Option<u16>,
 }
 
 impl Default for State {
@@ -15,7 +12,6 @@ impl Default for State {
             memory: [0; 1 << 16],
             registers: [0; 8],
             instruction_pointer: 0,
-            instruction_segment: None,
         }
     }
 }
@@ -24,12 +20,8 @@ impl Iterator for State {
     type Item = u8;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.instruction_pointer > self.instruction_segment? {
-            return None;
-        }
-
         let byte = self.memory.get(self.instruction_pointer as usize);
-        self.instruction_pointer += 1;
+        self.instruction_pointer = self.instruction_pointer.checked_add(1)?;
         byte.copied()
     }
 }
@@ -40,17 +32,11 @@ pub struct Processor {
 
 impl Processor {
     pub fn load(&mut self, program: impl Iterator<Item = u8>) {
-        let mut segment_end = 0;
         self.state
             .memory
             .iter_mut()
             .zip(program)
-            .enumerate()
-            .for_each(|(idx, (mem, prog))| {
-                *mem = prog;
-                segment_end = idx
-            });
-        self.state.instruction_segment = Some(segment_end as u16);
+            .for_each(|(mem, prog)| *mem = prog);
     }
 }
 
